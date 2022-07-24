13dd3a25677c99cbe7110587ab8237400f70d87f
Henri <razanabelahyhenri@gmail.com>
2022-07-25 00:06:21 +0300

    liste stagiaire dash

--- vue/Tache.unique.categ.php
+++ vue/Tache.unique.categ.php
@@ -7,7 +7,10 @@
     $connecte = $stg_connecter -> suisConnecter($_SESSION["stagiaire_connecter"]);
     $tache_stg_con = new TacheControler();
     $aff_propre_tache = $tache_stg_con -> listeTachesParCategorie($connecte['id_categorie']);
-    var_dump($aff_propre_tache);
+
+
+
+    //var_dump($aff_propre_tache);
     // foreach ($aff_propre_tache as $key => $value) {
     //     echo("io oh " . $value['description_tache'] . "<br>");
     // }
@@ -20,38 +23,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acc dash Stagiaire</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js">
+   
+  </script>
+    <style>
+       
+#rowval {
+    margin-left: 10px;
+}
+    </style>
 </head>
 <body>
+
+<div class="container">
+<div class="row">
     <?php
         foreach ($aff_propre_tache as $key => $value) 
         {?>
-        
-            <div>
-                <div>
-                    <h1>
-                        <?php
-                            echo($value['titre_tache']);
-                        ?>
-                    </h1>
-                </div>
-                <div>
-                    <p>
-                        <?php
-                            echo($value['description_tache']);
-                        ?> 
-                    </p>
-                </div>
-                <div>
-                    <h4>
-                        <?php
-                            echo($value['dure_tache']);
-                        ?>
-                        heure
-                    </h4>
-                </div>
-            </div>
+           
+              
+                    <div class="col-md-4">
+                        <div class="card" style="width:320px"  id="rowval">
+                            <img class="card-img-top" src="../images/fond.jpg" alt="Card image" style="width:100%">
+                        <div class="card-body">
+                            <h4 class="card-title"><?php echo($value['titre_tache']);?></h4>
+                            <p class="card-text"><?php echo($value['description_tache']);?></p>
+                            <a href="#" class="btn btn-primary stretched-link"><?php echo($value['dure_tache']);?></a>
+                        </div>
+                    </div>
+                        </div>
+                    
+              </br></br>
+           
         <?php
         }
     ?>
+
+</div>
+</div>
 </body>
 </html>
