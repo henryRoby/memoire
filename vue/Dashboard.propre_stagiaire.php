d2db915b772fb7b318a570ea433eb2e2ddf13799
Henri <razanabelahyhenri@gmail.com>
2022-07-24 12:36:30 +0300

    dimanche maraina ndray zao

--- vue/Dashboard.propre_stagiaire.php
+++ vue/Dashboard.propre_stagiaire.php
@@ -234,6 +234,7 @@ h1 {
                 </ul>
             </li>
         </ul>
+    
         <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav side-nav">
